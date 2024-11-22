02b24c1bc719c161c26de0199ac4639f15af012c
Dany <dany.dudiot@gmail.com>
2024-11-22 16:10:08 +0100

    début du contrôleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,9 +1,21 @@
 package controleur;
 
+import modele.Board;
+import vue.Ihm;
+
 public class Controleur {
+    protected Ihm ihm;
+    protected Board board;
+    public Controleur() {
+        this.ihm = Ihm();
+    }
 
     public void startGame(){
-
+        if (ihm.askBoard()){
+            this.board = new Board();
+        } else {
+            this.board = new Board(ihm.askFile());
+        }
     }
 
 }
