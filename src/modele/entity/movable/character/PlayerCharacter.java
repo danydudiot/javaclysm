c9c7f69db5a5b998ffc1af12531a4cc129bcc2e2
Dany <dany.dudiot@gmail.com>
2024-11-26 17:52:27 +0100

    Gestion des mouvement et direction du joueur only

--- src/modele/entity/movable/character/PlayerCharacter.java
+++ src/modele/entity/movable/character/PlayerCharacter.java
@@ -1,8 +1,37 @@
 package modele.entity.movable.character;
 
+import modele.Board;
+
 public class PlayerCharacter extends Character{
+    private char direction;
     public PlayerCharacter(int x, int y) {
         super(x, y);
         this.representation = "@";
+        this.direction = 'z';
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+
+    @Override
+    public void move(char direction, Board board) {
+        super.move(direction, board);
+        changeDirection(direction);
+    }
+
+    public void changeDirection(char direction){
+        if ("oklm".indexOf(direction) != -1){
+            this.direction = switch (direction) {
+                case 'o' -> 'z';
+                case 'k' -> 'q';
+                case 'l' -> 's';
+                case 'm' -> 'd';
+                default -> throw new IllegalStateException("Unexpected value: " + direction);
+            };
+        } else if ("zqsd".indexOf(direction) != -1){
+            this.direction = direction;
+        }
     }
 }
