da5eb387add336844cc1e6d8c6b781401492238e
kaawan <kaawan.jdr@gmail.com>
2024-11-27 17:03:02 +0100

    various fix to the IHM

--- src/vue/Ihm.java
+++ src/vue/Ihm.java
@@ -8,6 +8,7 @@ public class Ihm {
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_RED = "\u001B[31m";
 	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	private final int displayHeight;
@@ -28,9 +29,9 @@ public class Ihm {
 			System.out.println("Voulez vous charger une carte au format "+ ANSI_YELLOW+".txt"+ANSI_RESET +" ? (y/n)");
 			//String answer = sc.next(".");
 			String answer = sc.nextLine();
-			if (answer.equals("y")) {
+			if (answer.equals("y") || answer.equals("Y")) {
 				return true;
-			} else if (answer.equals("n")) {
+			} else if (answer.equals("n") || answer.equals("N")) {
 				return false;
 			} else {
 				System.out.println(ANSI_RED + "caractère non reconnu." + ANSI_RESET);
@@ -41,6 +42,9 @@ public class Ihm {
 		while (true) {
 			System.out.println("Veuillez entrer un path valide vers votre carte: ");
 			String answer = sc.nextLine();
+			if (answer.equals("c")) {
+				return "carte.txt";
+			}
 			if (answer.endsWith(".txt") || answer.endsWith(".TXT")) {
 				return answer;
 			} else {
@@ -64,7 +68,7 @@ public class Ihm {
 		String croppedBoard = cropBoard(board, boardWidth, boardHeight, displayWidth, displayHeight,playerX,playerY, true);
 		String ui = makeUi(displayWidth, actionHistory, playerX, playerY, playerDir, equippedItem);
 		System.out.println(croppedBoard);
-		System.out.println(ui);
+		System.out.print(ui);
 	}
 	private int clamp(int value, int low, int high) {
 		return Math.min(Math.max(low, value), high);
@@ -84,7 +88,7 @@ public class Ihm {
 		if (border) {
 			targetWidth -= 2;
 			targetHeight -= 2;
-			output.append("+").append("-".repeat(targetWidth)).append("+");
+			output.append("┌").append("─".repeat(targetWidth)).append("┐");
 		}
 
 		String[] lines = sourceString.split("\n");
@@ -96,26 +100,26 @@ public class Ihm {
 				output.append('\n');
 			}
 			if (border) {
-				output.append("|");
+				output.append("│");
 			}
 			int lineStart = offsetY + i;
 			int start = clamp(playerX - (targetWidth / 2), 0,sourceWidth-targetWidth);
 			output.append(lines[lineStart], start, start + targetWidth);
 			if (border) {
-				output.append("|");
+				output.append("│");
 			}
 		}
 		if (border) {
-			output.append("\n").append("+").append("-".repeat(targetWidth)).append("+");
+			output.append("\n").append("├").append("─".repeat(22)).append("┬").append("─".repeat(targetWidth-23)).append("┘");
 		}
 		return output.toString();
 	}
 	private String makeUi(int targetWidth, Queue<String> actionHistory, int playerX, int playerY, char playerDir, String equippedItem) {
 		Queue<String> actionHistoryCopy = new ArrayDeque<>(actionHistory);
-		return 	"| x: " + String.format("%-4S", playerX) +" y: " + String.format("%-4S", playerY) +"| " + actionHistoryCopy.remove() + '\n' +
-				"| Dir : (" + asArrow(playerDir) +")     | "+ actionHistoryCopy.remove() + '\n' +
-				"| >> " + String.format("%-12s", equippedItem) + "| " + actionHistoryCopy.remove() + '\n' +
-				ANSI_WHITE_BACKGROUND + String.format(("%-"+(targetWidth-1)+"s"), "ZQSD : Move    OKLM : Look    I : Inventaire    E : Interagir") + ANSI_RESET;
+		return 	"│ x: " + String.format("%-4S", playerX) +"y: " + String.format("%-4S", playerY) +"       │ " + actionHistoryCopy.remove() + '\n' +
+				"│ Dir : (" + asArrow(playerDir) +")            │ "+ actionHistoryCopy.remove() + '\n' +
+				"│ >> " + String.format("%-18s", equippedItem) + "│ " + actionHistoryCopy.remove() + '\n' +
+				ANSI_WHITE_BACKGROUND + ANSI_BLACK + String.format(("%-"+(targetWidth-1)+"s"), " ZQSD : Move    OKLM : Look    I : Inventaire    E : Interagir") + ANSI_RESET;
 //		| x: 14  y: 36   | Player move up
 //		| Dir : (↓)      | Player look left
 //		| >> Ecureil     | Monkey is now friend w/ Player
