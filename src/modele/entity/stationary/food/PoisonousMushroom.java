89dfdc8e4c5018b60c9a6a2f5345e0652728b605
Dany <dany.dudiot@gmail.com>
2024-12-27 10:02:04 +0100

    Ajout new class Food

---
+++ src/modele/entity/stationary/food/PoisonousMushroom.java
@@ -0,0 +1,11 @@
+package modele.entity.stationary.food;
+
+import modele.Colors;
+
+public class PoisonousMushroom extends Food{
+    public PoisonousMushroom(int x, int y) {
+        super(x,y);
+        this.representation = Colors.ANSI_YELLOW + "C" + Colors.ANSI_RESET;
+        this.displayName = "Champignon vénéneux";
+    }
+}
