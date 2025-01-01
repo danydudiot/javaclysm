84614bd25a73e8a12f97f7619ef3d58bb3965b96
Dany <dany.dudiot@gmail.com>
2025-01-01 23:47:27 +0100

    Création PredatorMoveCoordinateCommand

---
+++ src/modele/clock/commands/PredatorMoveCoordinateCommand.java
@@ -0,0 +1,34 @@
+package modele.clock.commands;
+
+import modele.Board;
+import modele.entity.movable.character.npc.predator.Predator;
+import modele.entity.movable.character.npc.state.State;
+import modele.entity.stationary.terrain.Terrain;
+
+public class PredatorMoveCoordinateCommand implements Command {
+	Predator predator;
+	Terrain terrain;
+	Terrain old_terrain;
+	State old_state;
+
+	public PredatorMoveCoordinateCommand(Predator predator, Terrain terrain) {
+		this.predator = predator;
+		this.terrain 	= terrain;
+		this.old_terrain = Board.getInstance().getAt(predator.getX(), predator.getY());
+		this.old_state 	= predator.getCurrentState();
+	}
+
+	@Override
+	public void doCommand() {
+		predator.move(terrain);
+	}
+
+	@Override
+	public void undoCommand() {
+		predator.setCurrentState(old_state);
+		predator.move(old_terrain);
+	}
+
+
+
+}
